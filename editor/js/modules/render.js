82fc4d8adb7c47f7f545ac8df4798e194668c4fc
Kyle <Kyle@realmaxdemos.com>
2017-11-01 03:49:37 -0700

    fix merge conflict

--- editor/js/modules/render.js
+++ editor/js/modules/render.js
@@ -169,6 +169,7 @@ var RenderModule = {
 			// KB: had to change for headtracking secret feature
 			if (v !== 'HeadTracking')
 				HeadtrackingModule.Stop();
+			
 			switch( v )
 			{
 				case "Perspective": 
